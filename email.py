f45319bb33fdee7bb713a6e4e956a0d1232f0b02
Ashu <jains9269@gmail.com>
2019-05-20 23:30:34 +0530

    stable version with pyperclip feature

--- email.py
+++ email.py
@@ -1,23 +1,21 @@
+#! /usr/bin/python3
 import re, pyperclip
 
 #email regex
-
 emailRegex = re.compile(r'''(
 	[a-zA-Z0-9._%+-]+	#username
 	@					#@ symbol
 	[a-zA-Z0-9.-]+		#domain name
-	(\.[a-zA-Z]{2,5}) 	#dot something
+	\.[a-zA-Z]{2,5} 	#dot something
 	)''', re.VERBOSE)
 
 #find matches in clipboard
-
 text = str(pyperclip.paste())
 matches = []
-for groups in emailRegex.findall(text):
-	matches.append(groups[0])
+for email in emailRegex.findall(text):
+	matches.append(email)
 
 #paste matches in clipboard
-
 if len(matches) > 0:
 	pyperclip.copy('\n'.join(matches))
 	print('\n'.join(matches))
